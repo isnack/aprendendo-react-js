67a9e9a1186d4d8824ca93cd5c1c338c02044e11
Isnack <isnack_yagami@hotmail.com>
2017-05-27 16:06:26 -0300

    Mudando url para a api do projeto do tcc

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,41 +1,45 @@
-    import React, {Component} from 'react';
+    import React, {
+        Component
+    }
+    from 'react';
     import axios from 'axios';
 
-    class Login extends Component{
+    class Login extends Component {
         constructor() {
-        super()
-        this.state ={
-                        data:'',
-                    };
+            super()
+            this.state = {
+                data: '',
+            };
         }
-        login(form){
-            var data={
-                        usuario:form.user.value,
-                        senha:form.password.value
-                     }
-            var th= this;
+        login(e, form) {
+
+            var usuario = {
+                usuario: form.user.value,
+                senha: form.password.value
+            }
+           
 
 
-         axios
-        .get('https://api.github.com/users/' + 'isnack')
-        .then(response => th.setState({data: response.data.url}))
-        .catch(err => console.log(err))
+            axios
+                .post('http://localhost:3004/api/authentication',usuario)
+                .then(response => this.setState({
+                    data: response.data
+                }))
+                .catch(err => console.log(err))
+            e.preventDefault();
         }
 
-        render(){
-            return(
-            <div>
-               <form >
-                <label for="usuario">Usuário</label>
-                <input ref='user' type="text"/>
-                <label for="Senha">Senha</label>
-                <input ref='password' type="text"/>
-                <button onClick={()=>this.login(this.refs)}>Entrar</button>
-                </form>
-                 <div>{this.state.data}</div>
-                
-            </div>
-            
+        render() {
+            return ( < div >
+                < form >
+                < label for = "usuario" > Usuário < /label> < input ref = 'user'type = "text" / >
+                < label for = "Senha" > Senha < /label> < input ref = 'password'type = "text" / >
+                < button onClick = {(e) => this.login(e, this.refs)} > Entrar < /button>
+                < /form>
+                < div > {this.state.data.message} < /div>
+
+                < /div>
+
             )
         }
 
